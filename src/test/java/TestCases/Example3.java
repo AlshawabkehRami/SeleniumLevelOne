e3d6b91ab0fc03330317ed4890fcc071cf648c67
rami <alshawabkeh.rami.1987@gmail.com>
2018-11-05 20:57:17 +0200

    add

---
+++ src/test/java/TestCases/Example3.java
@@ -0,0 +1,94 @@
+package TestCases;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+
+public class Example3 {
+
+    WebDriver driver;
+    WebDriverWait Wait;
+
+    @BeforeMethod
+    public void SetUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        Wait =new WebDriverWait(driver,10);
+
+        driver.navigate().to("https://demo.openmrs.org/openmrs/login.htm");
+
+
+    }
+
+    @Test(priority = 1)
+    public void Login() {
+
+        driver.findElement(By.id("username")).sendKeys("Admin");
+        driver.findElement(By.id("password")).sendKeys("Admin123");
+        driver.findElement(By.id("Inpatient Ward")).click();
+        driver.findElement(By.id("loginButton")).click();
+
+    }
+
+    @Test(priority = 2, dependsOnMethods = {"Login"})
+    public void Logout() {
+        Login();
+
+        driver.findElement(By.cssSelector("[href*=\"logout\"]"));
+
+
+    }
+
+    @Test(priority = 3, dependsOnMethods = {"Login"})
+    public void RegisteraPatient() {
+        Login();
+        driver.findElement(By.cssSelector("[id*=referenceapplication-registr]")).click();
+        if (!driver.findElement(By.cssSelector("[id=checkbox-unknown-patient]")).isSelected()){
+            driver.findElement(By.cssSelector("[id=checkbox-unknown-patient]")).click();
+
+        }
+
+
+    }
+
+    /*@Test(priority = 4)
+    public void FindPatientRecord() {
+
+
+    }
+
+    @Test(priority = 5)
+    public void ViewthePatient() {
+
+
+    }
+
+    @Test(priority = 6)
+    public void SearchaPatient() {
+
+
+    }
+
+    @Test(priority = 7)
+    public void BookanAppointment() {
+
+
+    }
+
+    @Test(priority = 8)
+    public void CaptureVitals() {
+
+
+    }
+*/
+
+    @AfterMethod
+    public void closeDriver() {
+       // driver.quit();
+
+    }
+
+}
