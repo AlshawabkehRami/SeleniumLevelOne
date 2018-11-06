21069afe0c997ef75896a9569e3e41393ad9b554
rami <alshawabkeh.rami.1987@gmail.com>
2018-11-06 21:06:34 +0200

    sss

--- src/test/java/TestCases/seleniumEasy.java
+++ src/test/java/TestCases/seleniumEasy.java
@@ -1,10 +1,22 @@
 package TestCases;
+
 import Global.SetUpBrowser;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 
 public class seleniumEasy extends SetUpBrowser {
+
+    private static final Logger logger = LoggerFactory.getLogger(seleniumEasy.class);
 
     @BeforeMethod
     public void SetStartUp() {
@@ -12,13 +24,31 @@ public class seleniumEasy extends SetUpBrowser {
     }
 
     @Test
-    private void test1() {
+    private void get_emp_Salary() {
+        logger.info("*********************************************************");
+        logger.info("GetSalary");
+        logger.info("*********************************************************");
+        driver.get("https://www.seleniumeasy.com/test/table-sort-search-demo.html");
+
+
+
+        Select DDL = new Select(driver.findElement(By.name("example_length")));
+        DDL.selectByVisibleText("100");
+
+        List<WebElement> TabeRows = driver.findElements(By.xpath("//*[@id=\"example\"]/thead"));
+        System.out.println(TabeRows.size());
+        for (int i = 0; i < TabeRows.size(); i++) {
+            String Name = TabeRows.get(i).getText();
+            System.out.println(Name);
+
+        }
+
 
     }
 
     @AfterMethod
     public void closeDriver() {
-        driver.quit();
+        //driver.quit();
 
     }
 }
